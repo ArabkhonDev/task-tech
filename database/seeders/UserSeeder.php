0deb8d8f044bc1d26ea9664871e91db69b6dcb78
ArabkhonDev <rightsolutionhub@gmail.com>
2025-02-03 12:58:12 +0500

    task project started 2025year 3feb

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'role_id'=>1,
+            'name'=>'Manager',
+            'email'=> 'arabjon@gmail.com',
+            'password'=>Hash::make('secret'),
+        ]);
+       for($i=1; $i<=10; $i++){
+        User::create([
+            'role_id'=>2,
+            'name'=>'Client',
+            'email'=> Str::random(8).'@gmail.com',
+            'password'=>Hash::make('test'),
+        ]);
+       }
+    }
+}
